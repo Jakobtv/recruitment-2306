addf12c52f0f5b7e853301c346ed21d8632d230e
Jakobtv <winklerjakob@icloud.com>
2024-04-18 17:27:58 +0200

    Add implementation of fast-route library from nikic

--- public/index.php
+++ public/index.php
@@ -9,8 +9,37 @@ ini_set('display_errors', '1');
 // autoloading
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// temporärer Code
-$controller = new App\Controller\HomeController();
-$controller->index(); 
+// Fast-Route-Library
 
-$controllera;
+
+// Dispatcher einrichten
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'App\Controller\HomeController::index');
+});
+
+// HTTP-Methode und URI abrufen
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// unnötige URL-Teile entfernen
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+// Routing abgleichen
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+        break;
+}
